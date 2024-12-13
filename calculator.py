d539d7986220af5ca94776426664d497a6dad97f
etse8 <etsehiwotgerbi@gmail.com>
2024-12-12 23:50:11 -0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,26 @@
+operators = ["+", "-", "*", "/"]
+
+# num1, opera, num2
+def calculator(expression):
+    for index in range(len(expression)):
+        if expression[index] in operators:
+            oper = expression[index]
+            num1 = int(expression[0:index])
+            num2 = int(expression[index+1:])
+
+    if oper == "+":
+        return num1 + num2
+    elif oper == "-":
+        return num1 + num2
+    elif oper == "*":
+        return num1 + num2
+    elif oper == "/":
+            return num1 / num2
+    
+    
+
+
+    
+
+
 
